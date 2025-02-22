e11dd2290d4a608a57ffa2f6e23bce10b56f18f1
Khanh Nguyen <khnguy3n@proton.me>
2025-02-22 12:09:48 -0500

    change color theme

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,9 +1,17 @@
- return {
-   "embark-theme/vim",
-   lazy = false,
-   priority = 1000,
-   opts = {},
-   config = function()
-     vim.cmd [[colorscheme embark]]
-   end
- }
+return {
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require("kanagawa").setup({
+			compile = false,
+			undercurl = true,
+			commentStyle = { italic = true },
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			transparent = false,
+			dimInactive = false,
+			terminalColors = true,
+			theme = "wave",
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
+}
